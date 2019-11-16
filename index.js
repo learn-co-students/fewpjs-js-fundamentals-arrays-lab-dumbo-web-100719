1c9069b3d727421d5c8957ad6567f5a733c1b9b2
Hamidou Diallo <Dio.hamidou@gmail.com>
2019-11-16 17:36:34 -0500

    Done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution here!
+
+// 1. Append the pet "Odie" to the end of`append`.
+// 2. Prepend the pet "Odie" to the beginning of`prepend`.
+// 3. Remove the _last_ pet from`removeLast`.
+// 4. Remove the _first_ pet from`removeFirst`.
+
+let append = ["Milo", "Otis", "Garfield"]
+append.push('Odie')
+
+let prepend = ["Milo", "Otis", "Garfield"]
+prepend.unshift('Odie')
+
+let removeLast = ["Milo", "Otis", "Garfield"]
+removeLast.pop()
+
+let removeFirst = ["Milo", "Otis", "Garfield"]
+removeFirst.shift()
+
+
